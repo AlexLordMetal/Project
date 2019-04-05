4f35ff848bf093a3d1c17b2081675e4c5287038d
AlexLordMetal <alexedbogdanov@gmail.com>
2019-04-05 18:05:33 +0300

    To git

---
+++ ServicesApp/ServicesApp.DataProvider/ApplicationDbContext.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNet.Identity.EntityFramework;
+using ServicesApp.DataProvider.IdentityModels;
+
+namespace ServicesApp.DataProvider
+{
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
+    {
+        public ApplicationDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static ApplicationDbContext Create()
+        {
+            return new ApplicationDbContext();
+        }
+    }
+}
---
+++ ServicesApp/ServicesApp.Website/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(ServicesApp.Website.Startup))]
+namespace ServicesApp.Website
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
