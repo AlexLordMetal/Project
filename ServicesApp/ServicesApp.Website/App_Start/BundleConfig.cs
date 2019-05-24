4463b105fc951fee5412159a46456b82b2d60b05
AlexLordMetal <alexedbogdanov@gmail.com>
2019-05-24 19:53:21 +0300

    some changes

--- ServicesApp/ServicesApp.Website/App_Start/BundleConfig.cs
+++ ServicesApp/ServicesApp.Website/App_Start/BundleConfig.cs
@@ -24,13 +24,13 @@ namespace ServicesApp.Website
                         "~/Scripts/modernizr-*"));
 
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
-                      "~/Scripts/bootstrap.min.js",
-                      "~/Scripts/bootstrap.bundle.min.js"));
+                      "~/Scripts/bootstrap.js",
+                      "~/Scripts/bootstrap.bundle.js"));
 
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.min.css",
-                      "~/Content/site.css",
-                      "~/Content/themes/base/jquery-ui.css"));
+                "~/Content/bootstrap.min.css",
+                "~/Content/site.css",
+                "~/Content/themes/base/jquery-ui.css"));
         }
     }
 }
